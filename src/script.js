9c16c4065bbd0958e93d4a74b5394ca28c440f94
kushkaftar <kushkaftar@gmail.com>
2020-03-27 08:37:20 +0300

    add lesson 2

--- src/script.js
+++ src/script.js
@@ -1,13 +1,17 @@
 // переменные
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 80000,
+    income = 'freelance',
+    addExpenses = 'cigarettes, wine, girls',
+    deposit = true,
+    mission = 1000,
+    period = 6;
 
-//модальное окно
-alert('Вывести на экран в модальном окне (alert) сообщение с любым текстом');
-
-//вывод в консоль
-console.log('Вывести в консоль сообщение с любым текстом');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
+console.log(addExpenses.toLocaleLowerCase().split(', '));
+budgetDay = money/30;
+console.log(budgetDay);
